b3eb80a653ba1b174df4b1aa7b230bfe9661f96b
madhan-swe <madhankumarcvkl@gmail.com>
2021-01-30 22:28:42 +0530

    User operations added - Registration, forgot password, login, account activation, get reser link, reset password

---
+++ customMiddleWares/authentication.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+function authorizeUser(req, res, next) {
+    if (req.headers.authorization !== undefined) {
+      jwt.verify(
+        req.headers.authorization,
+        process.env.JWT_KEY,
+        (err, decode) => {
+          
+          if(err && (err.toString()).includes("TokenExpiredError"))
+          {
+            res.status(401).json({
+                message: "Token Expired, Please login Again.",
+                result: false,
+                status: 401
+            })
+            return 
+          }
+          if (err) throw err;
+          if (decode) {
+            next();
+          } else {
+            res.status(403).json({result: false, message: "User not logged in", status: 403});
+          }
+        }
+      );
+    } else {
+        res.status(403).json({result: false, message: "User not logged in", status: 403});
+    }
+  }
+
+  module.exports = {authorizeUser};
